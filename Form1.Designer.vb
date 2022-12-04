0bb70f8c2d675573f4f9dd75a452694cebf90946
NIIBOYE25 <Rexwills20@gmail.com>
2022-12-04 14:32:03 +0000

    Initial commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,382 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.BtnER = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Lbl1 = New System.Windows.Forms.Label()
+        Me.lbl2 = New System.Windows.Forms.Label()
+        Me.lbl3 = New System.Windows.Forms.Label()
+        Me.lbl4 = New System.Windows.Forms.Label()
+        Me.engbox1 = New System.Windows.Forms.TextBox()
+        Me.mathsbox2 = New System.Windows.Forms.TextBox()
+        Me.sciencebox3 = New System.Windows.Forms.TextBox()
+        Me.socialbox4 = New System.Windows.Forms.TextBox()
+        Me.btnclear = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.gradeBox1 = New System.Windows.Forms.TextBox()
+        Me.gradeBox2 = New System.Windows.Forms.TextBox()
+        Me.gradebox3 = New System.Windows.Forms.TextBox()
+        Me.gradeBox4 = New System.Windows.Forms.TextBox()
+        Me.Btncal = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Dbox1 = New System.Windows.Forms.Label()
+        Me.Dbox2 = New System.Windows.Forms.Label()
+        Me.Dbox3 = New System.Windows.Forms.Label()
+        Me.Dbox4 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'BtnER
+        '
+        Me.BtnER.BackColor = System.Drawing.Color.LightSteelBlue
+        Me.BtnER.Font = New System.Drawing.Font("MS Reference Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.BtnER.Location = New System.Drawing.Point(286, 317)
+        Me.BtnER.Name = "BtnER"
+        Me.BtnER.Size = New System.Drawing.Size(142, 59)
+        Me.BtnER.TabIndex = 0
+        Me.BtnER.Text = "Enter " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Result"
+        Me.BtnER.UseVisualStyleBackColor = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI Semilight", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.Location = New System.Drawing.Point(286, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(274, 30)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "RESULT GRADING SYSTEM"
+        '
+        'Lbl1
+        '
+        Me.Lbl1.AutoSize = True
+        Me.Lbl1.Font = New System.Drawing.Font("Yu Gothic UI Semibold", 13.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Lbl1.Location = New System.Drawing.Point(110, 131)
+        Me.Lbl1.Name = "Lbl1"
+        Me.Lbl1.Size = New System.Drawing.Size(85, 25)
+        Me.Lbl1.TabIndex = 2
+        Me.Lbl1.Text = "ENGLISH"
+        '
+        'lbl2
+        '
+        Me.lbl2.AutoSize = True
+        Me.lbl2.Font = New System.Drawing.Font("Yu Gothic UI Semibold", 13.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.lbl2.Location = New System.Drawing.Point(110, 167)
+        Me.lbl2.Name = "lbl2"
+        Me.lbl2.Size = New System.Drawing.Size(136, 25)
+        Me.lbl2.TabIndex = 3
+        Me.lbl2.Text = "MATHEMATICS"
+        '
+        'lbl3
+        '
+        Me.lbl3.AutoSize = True
+        Me.lbl3.Font = New System.Drawing.Font("Yu Gothic UI Semibold", 13.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.lbl3.Location = New System.Drawing.Point(110, 206)
+        Me.lbl3.Name = "lbl3"
+        Me.lbl3.Size = New System.Drawing.Size(186, 25)
+        Me.lbl3.TabIndex = 4
+        Me.lbl3.Text = "INTEGRATED SCENCE"
+        '
+        'lbl4
+        '
+        Me.lbl4.AutoSize = True
+        Me.lbl4.Font = New System.Drawing.Font("Yu Gothic UI Semibold", 13.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.lbl4.Location = New System.Drawing.Point(110, 242)
+        Me.lbl4.Name = "lbl4"
+        Me.lbl4.Size = New System.Drawing.Size(148, 25)
+        Me.lbl4.TabIndex = 5
+        Me.lbl4.Text = "SOCIAL STUDIES"
+        '
+        'engbox1
+        '
+        Me.engbox1.BackColor = System.Drawing.Color.RosyBrown
+        Me.engbox1.Font = New System.Drawing.Font("Segoe UI Semibold", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.engbox1.Location = New System.Drawing.Point(354, 129)
+        Me.engbox1.Name = "engbox1"
+        Me.engbox1.Size = New System.Drawing.Size(50, 27)
+        Me.engbox1.TabIndex = 6
+        Me.engbox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'mathsbox2
+        '
+        Me.mathsbox2.BackColor = System.Drawing.Color.RosyBrown
+        Me.mathsbox2.Font = New System.Drawing.Font("Segoe UI Semibold", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.mathsbox2.Location = New System.Drawing.Point(354, 169)
+        Me.mathsbox2.Name = "mathsbox2"
+        Me.mathsbox2.Size = New System.Drawing.Size(50, 27)
+        Me.mathsbox2.TabIndex = 7
+        Me.mathsbox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'sciencebox3
+        '
+        Me.sciencebox3.BackColor = System.Drawing.Color.RosyBrown
+        Me.sciencebox3.Font = New System.Drawing.Font("Segoe UI Semibold", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.sciencebox3.Location = New System.Drawing.Point(354, 208)
+        Me.sciencebox3.Name = "sciencebox3"
+        Me.sciencebox3.Size = New System.Drawing.Size(50, 27)
+        Me.sciencebox3.TabIndex = 8
+        Me.sciencebox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'socialbox4
+        '
+        Me.socialbox4.BackColor = System.Drawing.Color.RosyBrown
+        Me.socialbox4.Font = New System.Drawing.Font("Segoe UI Semibold", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.socialbox4.Location = New System.Drawing.Point(354, 247)
+        Me.socialbox4.Name = "socialbox4"
+        Me.socialbox4.Size = New System.Drawing.Size(50, 27)
+        Me.socialbox4.TabIndex = 9
+        Me.socialbox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'btnclear
+        '
+        Me.btnclear.BackColor = System.Drawing.Color.LightSteelBlue
+        Me.btnclear.Font = New System.Drawing.Font("MS Reference Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.btnclear.Location = New System.Drawing.Point(445, 325)
+        Me.btnclear.Name = "btnclear"
+        Me.btnclear.Size = New System.Drawing.Size(71, 42)
+        Me.btnclear.TabIndex = 10
+        Me.btnclear.Text = "clear"
+        Me.btnclear.UseVisualStyleBackColor = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label2.Location = New System.Drawing.Point(110, 71)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(98, 30)
+        Me.Label2.TabIndex = 11
+        Me.Label2.Text = "SUBJECT"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Segoe UI", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label3.Location = New System.Drawing.Point(331, 71)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(88, 30)
+        Me.Label3.TabIndex = 12
+        Me.Label3.Text = "MARKS"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Segoe UI", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label4.Location = New System.Drawing.Point(465, 71)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(83, 30)
+        Me.Label4.TabIndex = 13
+        Me.Label4.Text = "GRADE"
+        '
+        'gradeBox1
+        '
+        Me.gradeBox1.BackColor = System.Drawing.Color.RosyBrown
+        Me.gradeBox1.Font = New System.Drawing.Font("Segoe UI Semibold", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.gradeBox1.Location = New System.Drawing.Point(487, 130)
+        Me.gradeBox1.Name = "gradeBox1"
+        Me.gradeBox1.Size = New System.Drawing.Size(48, 27)
+        Me.gradeBox1.TabIndex = 14
+        Me.gradeBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'gradeBox2
+        '
+        Me.gradeBox2.BackColor = System.Drawing.Color.RosyBrown
+        Me.gradeBox2.Font = New System.Drawing.Font("Segoe UI Semibold", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.gradeBox2.Location = New System.Drawing.Point(487, 169)
+        Me.gradeBox2.Name = "gradeBox2"
+        Me.gradeBox2.Size = New System.Drawing.Size(48, 27)
+        Me.gradeBox2.TabIndex = 15
+        Me.gradeBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'gradebox3
+        '
+        Me.gradebox3.BackColor = System.Drawing.Color.RosyBrown
+        Me.gradebox3.Font = New System.Drawing.Font("Segoe UI Semibold", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.gradebox3.Location = New System.Drawing.Point(487, 208)
+        Me.gradebox3.Name = "gradebox3"
+        Me.gradebox3.Size = New System.Drawing.Size(48, 27)
+        Me.gradebox3.TabIndex = 16
+        Me.gradebox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'gradeBox4
+        '
+        Me.gradeBox4.BackColor = System.Drawing.Color.RosyBrown
+        Me.gradeBox4.Font = New System.Drawing.Font("Segoe UI Semibold", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.gradeBox4.Location = New System.Drawing.Point(487, 247)
+        Me.gradeBox4.Name = "gradeBox4"
+        Me.gradeBox4.Size = New System.Drawing.Size(48, 27)
+        Me.gradeBox4.TabIndex = 17
+        Me.gradeBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Btncal
+        '
+        Me.Btncal.BackColor = System.Drawing.Color.LightSteelBlue
+        Me.Btncal.Font = New System.Drawing.Font("MS Reference Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Btncal.ImageAlign = System.Drawing.ContentAlignment.BottomCenter
+        Me.Btncal.Location = New System.Drawing.Point(538, 317)
+        Me.Btncal.Name = "Btncal"
+        Me.Btncal.Size = New System.Drawing.Size(134, 59)
+        Me.Btncal.TabIndex = 18
+        Me.Btncal.Text = "Calculate Grade"
+        Me.Btncal.UseVisualStyleBackColor = False
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.LightSteelBlue
+        Me.Button1.Font = New System.Drawing.Font("MS Reference Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button1.Location = New System.Drawing.Point(713, 479)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 31)
+        Me.Button1.TabIndex = 19
+        Me.Button1.Text = "EXIT"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Segoe UI Semibold", 9.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point)
+        Me.Label5.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Label5.Location = New System.Drawing.Point(3, 504)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(122, 15)
+        Me.Label5.TabIndex = 20
+        Me.Label5.Text = "-Powered By Willisoft"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Segoe UI", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label6.Location = New System.Drawing.Point(576, 71)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(146, 30)
+        Me.Label6.TabIndex = 21
+        Me.Label6.Text = "DISCRIPTION"
+        '
+        'Dbox1
+        '
+        Me.Dbox1.AutoSize = True
+        Me.Dbox1.Font = New System.Drawing.Font("Segoe UI", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Dbox1.Location = New System.Drawing.Point(619, 130)
+        Me.Dbox1.Name = "Dbox1"
+        Me.Dbox1.Size = New System.Drawing.Size(0, 20)
+        Me.Dbox1.TabIndex = 22
+        '
+        'Dbox2
+        '
+        Me.Dbox2.AutoSize = True
+        Me.Dbox2.Font = New System.Drawing.Font("Segoe UI", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Dbox2.Location = New System.Drawing.Point(619, 170)
+        Me.Dbox2.Name = "Dbox2"
+        Me.Dbox2.Size = New System.Drawing.Size(0, 20)
+        Me.Dbox2.TabIndex = 23
+        '
+        'Dbox3
+        '
+        Me.Dbox3.AutoSize = True
+        Me.Dbox3.Font = New System.Drawing.Font("Segoe UI", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Dbox3.Location = New System.Drawing.Point(619, 210)
+        Me.Dbox3.Name = "Dbox3"
+        Me.Dbox3.Size = New System.Drawing.Size(0, 20)
+        Me.Dbox3.TabIndex = 24
+        '
+        'Dbox4
+        '
+        Me.Dbox4.AutoSize = True
+        Me.Dbox4.Font = New System.Drawing.Font("Segoe UI", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Dbox4.Location = New System.Drawing.Point(619, 248)
+        Me.Dbox4.Name = "Dbox4"
+        Me.Dbox4.Size = New System.Drawing.Size(0, 20)
+        Me.Dbox4.TabIndex = 25
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.RebeccaPurple
+        Me.ClientSize = New System.Drawing.Size(800, 522)
+        Me.Controls.Add(Me.Dbox4)
+        Me.Controls.Add(Me.Dbox3)
+        Me.Controls.Add(Me.Dbox2)
+        Me.Controls.Add(Me.Dbox1)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Btncal)
+        Me.Controls.Add(Me.gradeBox4)
+        Me.Controls.Add(Me.gradebox3)
+        Me.Controls.Add(Me.gradeBox2)
+        Me.Controls.Add(Me.gradeBox1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.btnclear)
+        Me.Controls.Add(Me.socialbox4)
+        Me.Controls.Add(Me.sciencebox3)
+        Me.Controls.Add(Me.mathsbox2)
+        Me.Controls.Add(Me.engbox1)
+        Me.Controls.Add(Me.lbl4)
+        Me.Controls.Add(Me.lbl3)
+        Me.Controls.Add(Me.lbl2)
+        Me.Controls.Add(Me.Lbl1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.BtnER)
+        Me.Name = "Form1"
+        Me.Text = "Grade Calculator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents BtnER As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Lbl1 As Label
+    Friend WithEvents lbl2 As Label
+    Friend WithEvents lbl3 As Label
+    Friend WithEvents lbl4 As Label
+    Friend WithEvents engbox1 As TextBox
+    Friend WithEvents mathsbox2 As TextBox
+    Friend WithEvents sciencebox3 As TextBox
+    Friend WithEvents socialbox4 As TextBox
+    Friend WithEvents Btncal As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Txtsocial As Label
+    Friend WithEvents txtscience As Label
+    Friend WithEvents btnclear As Button
+    Friend WithEvents gradeBox1 As TextBox
+    Friend WithEvents gradeBox2 As TextBox
+    Friend WithEvents gradebox3 As TextBox
+    Friend WithEvents gradeBox4 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Dbox1 As Label
+    Friend WithEvents Dbox2 As Label
+    Friend WithEvents Dbox3 As Label
+    Friend WithEvents Dbox4 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,247 @@
+﻿Public Class Form1
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+
+
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles BtnER.Click
+        Dim ENGLISH, SCIENCE, SOCIAL, MATHEMATICS As Integer
+
+
+        'INPUT
+        ENGLISH = InputBox("Please Enter English score:")
+        SCIENCE = InputBox("Please Enter Science score:")
+        SOCIAL = InputBox("Please Enter Social Score:")
+        MATHEMATICS = InputBox("Please Enter Mathematics score:")
+
+
+        'ADDING VALUES FROM MARKS BOX TO TEXT BOX
+        engbox1.Text = ENGLISH
+        sciencebox3.Text = SCIENCE
+        socialbox4.Text = SOCIAL
+        mathsbox2.Text = MATHEMATICS
+
+
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles btnclear.Click
+
+        'Clear
+        engbox1.Clear()
+        mathsbox2.Clear()
+        socialbox4.Clear()
+        sciencebox3.Clear()
+
+        gradeBox1.Clear()
+        gradeBox2.Clear()
+        gradebox3.Clear()
+        gradeBox4.Clear()
+
+
+
+    End Sub
+
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
+
+
+        'END
+
+        Dim Message As String = "Are you sure you want to Exit? "
+        Dim title As String = "Exit Program"
+        Dim result = MessageBox.Show(Message, title, MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question)
+
+        If result = DialogResult.Cancel Then
+        ElseIf result = DialogResult.No Then
+        ElseIf result = DialogResult.Yes Then
+            End
+        End If
+
+
+    End Sub
+
+    Private Sub Btncal_Click(sender As Object, e As EventArgs) Handles Btncal.Click
+
+        'Declearatons
+        Dim grade1, grade2, grade3, grade4 As String
+        Dim ENGLISH, SCIENCE, SOCIAL, MATHEMATICS As Integer
+        Dim D1, D2, D3, D4 As String
+
+
+        ENGLISH = engbox1.Text
+        SCIENCE = sciencebox3.Text
+        SOCIAL = socialbox4.Text
+        MATHEMATICS = mathsbox2.Text
+
+
+        grade1 = gradeBox1.Text
+        grade2 = gradeBox2.Text
+        grade3 = gradebox3.Text
+        grade4 = gradeBox4.Text
+
+        D1 = Dbox1.Text
+        D2 = Dbox2.Text
+        D3 = Dbox3.Text
+        D4 = Dbox4.Text
+
+        'Calculating grade for ENGLISH
+
+        If (ENGLISH >= 100) Then
+            gradeBox1.Text = "A"
+            Dbox1.Text = "EXCELLENT"
+
+        ElseIf (ENGLISH >= 80) Then
+            gradeBox1.Text = "B+"
+            Dbox1.Text = "VERY GOOD"
+
+        ElseIf (ENGLISH >= 70) Then
+            gradeBox1.Text = "B"
+            Dbox1.Text = "GOOD"
+
+        ElseIf (ENGLISH >= 60) Then
+            gradeBox1.Text = "C+"
+            Dbox1.Text = "AVERAGE"
+
+        ElseIf (ENGLISH >= 50) Then
+            gradeBox1.Text = "C"
+            Dbox1.Text = "FAIR"
+
+        ElseIf (ENGLISH >= 40) Then
+            gradeBox1.Text = "D+"
+            Dbox1.Text = "BARELY SATISFACTORY"
+
+        ElseIf (ENGLISH >= 30) Then
+            gradeBox1.Text = "D"
+            Dbox1.Text = "WEAK PASS"
+
+        ElseIf (ENGLISH >= 20) Then
+            gradeBox1.Text = "E"
+            Dbox1.Text = "FAIL"
+        End If
+
+
+        'Calculating grade for MATHEMATICS
+
+        If (MATHEMATICS >= 100) Then
+            gradeBox2.Text = "A"
+            Dbox2.Text = "EXCELLENT"
+
+        ElseIf (MATHEMATICS >= 80) Then
+            gradeBox2.Text = "B+"
+            Dbox2.Text = "VERY GOOD"
+
+        ElseIf (MATHEMATICS >= 70) Then
+            gradeBox2.Text = "B"
+            Dbox2.Text = "GOOD"
+
+        ElseIf (MATHEMATICS >= 60) Then
+            gradeBox2.Text = "C+"
+            Dbox2.Text = "AVERAGE"
+
+        ElseIf (MATHEMATICS >= 50) Then
+            gradeBox2.Text = "C"
+            Dbox2.Text = "FAIR"
+
+        ElseIf (MATHEMATICS >= 40) Then
+            gradeBox2.Text = "D+"
+            Dbox2.Text = "BARELY SATISFACTORY"
+
+        ElseIf (MATHEMATICS >= 30) Then
+            gradeBox2.Text = "D"
+            Dbox2.Text = "WEAK PASS"
+
+        ElseIf (MATHEMATICS >= 20) Then
+            gradeBox2.Text = "E"
+            Dbox2.Text = "FAIL"
+        End If
+
+
+
+        'Calculating grade for SCIENCE
+
+        If (SCIENCE >= 100) Then
+            gradebox3.Text = "A"
+            Dbox3.Text = "EXCELLENT"
+
+        ElseIf (SCIENCE >= 80) Then
+            gradebox3.Text = "B+"
+            Dbox3.Text = "VERY GOOD"
+
+        ElseIf (SCIENCE >= 70) Then
+            gradebox3.Text = "B"
+            Dbox3.Text = "GOOD"
+
+        ElseIf (SCIENCE >= 60) Then
+            gradebox3.Text = "C+"
+            Dbox3.Text = "AVERAGE"
+
+        ElseIf (SCIENCE >= 50) Then
+            gradebox3.Text = "C"
+            Dbox3.Text = "FAIR"
+
+        ElseIf (SCIENCE >= 40) Then
+            gradebox3.Text = "D+"
+            Dbox3.Text = "BARELY SATISFACTORY"
+
+        ElseIf (SCIENCE >= 30) Then
+            gradebox3.Text = "D"
+            Dbox3.Text = "WEAK PASS"
+
+        ElseIf (SCIENCE >= 20) Then
+            gradebox3.Text = "E"
+            Dbox3.Text = "FAIL"
+
+        End If
+
+
+
+        'Calculating grade for SOCIAL
+
+        If (SOCIAL >= 100) Then
+            gradeBox4.Text = "A"
+            Dbox4.Text = "EXCELLENT"
+
+        ElseIf (SOCIAL >= 80) Then
+            gradeBox4.Text = "B+"
+            Dbox4.Text = "VERY GOOD"
+
+        ElseIf (SOCIAL >= 70) Then
+            gradeBox4.Text = "B"
+            Dbox4.Text = "GOOD"
+
+        ElseIf (SOCIAL >= 60) Then
+            gradeBox4.Text = "C+"
+            Dbox4.Text = "AVERAGE"
+
+        ElseIf (SOCIAL >= 50) Then
+            gradeBox4.Text = "C"
+            Dbox4.Text = "FAIR"
+
+        ElseIf (SOCIAL >= 40) Then
+            gradeBox4.Text = "D+"
+            Dbox4.Text = "BARELY SATISFACTORY"
+
+        ElseIf (SOCIAL >= 30) Then
+            gradeBox4.Text = "D"
+            Dbox4.Text = "WEAK PASS"
+
+        ElseIf (SOCIAL >= 20) Then
+            gradeBox4.Text = "E"
+            Dbox4.Text = "FAIL"
+        End If
+
+    End Sub
+
+    Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles Me.FormClosing
+        Dim Message As String = "Are you sure you want to close? "
+        Dim title As String = "Close Program"
+        Dim result = MessageBox.Show(Message, title, MessageBoxButtons.OKCancel, MessageBoxIcon.Question)
+        If result = DialogResult.Cancel Then
+            e.Cancel = True
+        End If
+    End Sub
+End Class
